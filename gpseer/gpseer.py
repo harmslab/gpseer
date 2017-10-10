2074523912aae436b34ac36c7379ba944a5da011
Zachary Sailer <zachsailer@gmail.com>
2017-10-09 19:15:18 -0700

    flip GPseer argument to the correct way

--- gpseer/gpseer.py
+++ gpseer/gpseer.py
@@ -5,9 +5,9 @@ from .distributed import DistributedEngine
 def GPSeer(gpm, model, client=None, db_path="database/"):
     """Factory for sampling engines."""
     # Tell whether to serialize or not.
-    if client != None: 
+    if client == None: 
         cls = SerialEngine(gpm, model, db_path=db_path)
-    elif client == None:
+    elif client != None:
         cls = DistributedEngine(gpm, model, client=client, db_path=db_path)
     else:
         raise EngineError('client argument is invalid. Must be "serial" or "distributed".')
