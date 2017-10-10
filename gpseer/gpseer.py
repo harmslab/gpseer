68df6490e8d84b030feda628dd3ddf7fc728ee2a
Zachary Sailer <zachsailer@gmail.com>
2017-10-09 19:19:00 -0700

    reorder gpseer

--- gpseer/gpseer.py
+++ gpseer/gpseer.py
@@ -8,7 +8,7 @@ def GPSeer(gpm, model, client=None, db_path="database/"):
     if client == None: 
         cls = SerialEngine(gpm, model, db_path=db_path)
     elif client != None:
-        cls = DistributedEngine(gpm, model, client=client, db_path=db_path)
+        cls = DistributedEngine(client, gpm=gpm, model=model, db_path=db_path)
     else:
         raise EngineError('client argument is invalid. Must be "serial" or "distributed".')
     return cls
