48cde8c8a02134f798368b275fd973f935bb4036
Zsailer <zachsailer@gmail.com>
2020-04-02 15:41:56 -0700

    fix requirements

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,5 +5,15 @@ import pytest
 pytestmark = pytest.mark.script_launch_mode('subprocess')
 
 
-def test_help(script_runner, capsys):
+def test_estimate_ml(script_runner):
     script_runner.run('gpseer', 'estimate-ml')
+
+
+def test_goodness_of_fit(script_runner):
+    script_runner.run('gpseer', 'goodness-of-fit')
+
+
+def test_goodness_of_fit2(script_runner):
+    script_runner.run('gpseer', 'of-fit')
+
+
