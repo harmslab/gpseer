db7b36e4ce8f0a340e12e78a245fd7998503e355
Michael J. Harms <harmsm@gmail.com>
2020-04-10 15:39:32 -0700

    fixed residual bug

--- gpseer/plot.py
+++ gpseer/plot.py
@@ -177,6 +177,8 @@ def plot_correlation(model,prediction_df):
                            label=labels[i])
 
             # residual plot
+            residual = df_no)meas_err.prediction - df_no_meas_err.measured
+
             ax[1].errorbar(x=df_no_meas_err.measured,
                            y=residual,
                            fmt="o",color=colors[i],
